dfa7e2a0e6fd9674294ab8b16a2bbe6b76b181a2
alkaitz <alkaitz@gmail.com>
2017-07-29 10:34:14 -0700

    Upload empty project with Eclipse configuration

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # retro-emulator
 C++ emulator to investigate C++11 and C++14 features.
+
+So far, Eclipse workspace configuration is included in the repository. If contribution or multi-platform is considered, I will adapt the configuration.
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : alkaitz
+// Version     :
+// Copyright   : 
+// Description : Main emulator module
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	auto i = 5;
+	cout << "Hi" << endl; // prints Hi
+	return 0;
+}
